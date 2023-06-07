b1b8ee87e4e7ca51ad2d35a2997ab75f09bf803e
biplabroy1 <biplabroy7318@gmail.com>
2023-06-07 18:25:56 +0530

    Project Done but Not Optimize Code and Not Maintain MVC format

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+// const path = require('path');
+// const rootDir = path.dirname(require.main.filename);
+// const User = require('../models/users')
+
+// exports.getAppointmentPage = (req, res, next) => {
+//     res.sendFile(path.join(rootDir, "views", "index.html"));
+// }
